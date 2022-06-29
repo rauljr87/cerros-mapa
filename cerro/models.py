f7c3da4c0fb7a13848f65599db304de4cc2e5d32
r4u1 <rauljr87@hotmail.com>
2022-06-28 22:13:17 -0500

    model App cerros
    - created class cerro
    - created class latitud

--- cerro/models.py
+++ cerro/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Cerros(models.Model):
+    """ Tabla para nombres de cerros """
+
+    name = models.CharField('Nombre del Cerro', max_length=50)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
+
+
+class Latitud(models.Model):
+    """ Latitud de coordenada """
+
+    DIR_CHOICES = (
+        ('0', 'N'),
+        ('1', 'S'),
+    )
+    degrees = models.IntegerField('Grados')
+    minutes = models.IntegerField('Minutes')
+    seconds = models.IntegerField('Seconds')
+    direction = models.CharField('Directions', max_length=10, choices=DIR_CHOICES)
