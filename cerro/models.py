82e433cbfdecdd702d4221df7140a93a26729e2f
r4u1 <rauljr87@hotmail.com>
2022-06-28 22:55:08 -0500

    model class Cerro
    - created field latitud with one-to-one relationship
    - created field longitud with one-to-one relationship

--- cerro/models.py
+++ cerro/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-class Cerros(models.Model):
-    """ Tabla para nombres de cerros """
-
-    name = models.CharField('Nombre del Cerro', max_length=50)
-
-    def __str__(self):
-        return str(self.id) + ' - ' + self.name
-
-
 class Latitud(models.Model):
     """ Latitud de coordenada """
 
@@ -21,3 +12,43 @@ class Latitud(models.Model):
     minutes = models.IntegerField('Minutes')
     seconds = models.IntegerField('Seconds')
     direction = models.CharField('Directions', max_length=10, choices=DIR_CHOICES)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + str(self.degrees) + '°' + str(self.minutes) + """'""" + str(
+            self.seconds) + '''"''' + self.direction
+
+
+class Longitud(models.Model):
+    """ Longitud de coordenada """
+
+    DIR_CHOICES = (
+        ('0', 'E'),
+        ('1', 'W'),
+    )
+    degrees = models.IntegerField('Grados')
+    minutes = models.IntegerField('Minutes')
+    seconds = models.IntegerField('Seconds')
+    direction = models.CharField('Directions', max_length=10, choices=DIR_CHOICES)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + str(self.degrees) + '°' + str(self.minutes) + """'""" + str(
+            self.seconds) + '''"''' + self.direction
+
+
+class Cerros(models.Model):
+    """ Tabla para nombres de cerros """
+
+    name = models.CharField('Nombre del Cerro', max_length=50)
+    latitud = models.OneToOneField(
+        Latitud,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    longitud = models.OneToOneField(
+        Longitud,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
